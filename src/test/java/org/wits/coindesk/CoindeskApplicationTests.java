5c4de0f435333934e266f44bc853cafbf0dd4132
StevenHo <steven.ho@tpisoftware.com>
2023-04-17 11:13:58 +0800

    init commit

---
+++ src/test/java/org/wits/coindesk/CoindeskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wits.coindesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
